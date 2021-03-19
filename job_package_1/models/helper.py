d24e312e539d449992db6f696e9a1f2a31cf01d8
Mr-module <opensysknw@gmail.com>
2021-03-20 00:33:58 +0530

    [Changed] Variable Names to be more logical.

--- job_package_1/models/helper.py
+++ job_package_1/models/helper.py
@@ -1,71 +1,71 @@
 # -*- coding: utf-8 -*-
 
-def find_dynamic_tags(s, a, b): 
+def find_dynamic_tags(string, start, end): 
   
     # initially our result list is empty . 
-    res = list()
+    result = list()
 
     # find the length of given strings 
-    ls = len(s) 
-    la = len(a) 
-    lb = len(b)
+    string_length = len(string) 
+    start_length = len(start) 
+    end_length = len(end)
   
     # currently make array and initially 
     # put zero. 
-    x = [0] * ls
-    y = [0] * ls 
+    list_x = [0] * string_length
+    list_y = [0] * string_length 
   
     # find occurrence of "a" and "b" in string "s" 
-    for i in range(ls): 
+    for i in range(string_length): 
           
-        if (s[i: la + i] == a): 
-            x[i] = 1
-        if (s[i: lb + i] == b): 
-            y[i] = 1
+        if (string[i: start_length + i] == start): 
+            list_x[i] = 1
+        if (string[i: end_length + i] == end): 
+            list_y[i] = 1
   
-    # We use a hash to make sure that same  
+    # We use a non_duplicate_strings to make sure that same  
     # substring is not counted twice. 
-    hash = []  
+    non_duplicate_strings = list()
   
     # go through all the positions to find  
     # occurrence of "a" first. 
     curr_substr = "" 
-    for i in range(ls): 
+    for i in range(string_length): 
       
         # if we found occurrence of "a". 
-        if (x[i]): 
+        if (list_x[i]): 
           
             # then go through all the positions 
             # to find occurrence of "b". 
-            for j in range( i, ls): 
+            for j in range( i, string_length): 
               
                 # if we do found "b" at index 
                 # j then add it to already 
                 # existed substring. 
-                if (not y[j]): 
-                    curr_substr += s[j] 
+                if (not list_y[j]): 
+                    curr_substr += string[j] 
   
                 # if we found occurrence of "b". 
-                if (y[j]): 
+                if (list_y[j]): 
                   
                     # now add string "b" to  
                     # already existed substing. 
                   
-                    curr_substr += s[j: lb + j] 
+                    curr_substr += string[j: end_length + j] 
                       
                     # If current substring is not 
                     # included already. 
-                    if curr_substr not in hash: 
-                        if curr_substr.count(a) == 1:
-                            res.append(curr_substr)
+                    if curr_substr not in non_duplicate_strings: 
+                        if curr_substr.count(start) == 1:
+                            result.append(curr_substr)
   
                     # put any non negative integer  
                     # to make this string as already  
                     # existed. 
-                    hash.append(curr_substr) 
+                    non_duplicate_strings.append(curr_substr) 
   
-            # make substring null. 
+            # make substring null.
             curr_substr = "" 
   
     # return result. 
-    return res 
+    return result
